383d6819f9dde6be3aa7b64c2bf7e2deadfde75d
DESKTOP-OADF5ET\gajus <gajus.zabulis@gmail.com>
2021-12-13 20:39:38 +0200

    design v2

--- Src/index.php
+++ Src/index.php
@@ -9,6 +9,19 @@ require("functions.php");
 <head>
 <title>Page Title</title>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+<style>
+
+body{
+  font-family: 'Montserrat', sans-serif;
+}
+.textbold{
+  font-weight: bold;
+}
+</style>
+
 </head>
 <body>
 
@@ -20,11 +33,21 @@ if(!isset($_SESSION['Current_user_ID'])) $_SESSION['Current_user_ID'] = 0;
 echo '<p> Current userId: '.$_SESSION['Current_user_ID'] .'</p>'; // should be hidden from users, now only for debugging
 if($_SESSION['Current_user_ID'] != 0)
 {
-    echo '<p>'.$_SESSION['User_Name'].' balance:'.$_SESSION['Currency_Balance'].' coins, art pecies owned: '.$_SESSION['Owned_Art_Amount'].'</p>';
+  echo '<div class="w3-row w3-padding w3-border w3-pale-blue w3-padding-16 w3-wide w3-large"  style="margin:auto;">
+  <div class="w3-card-3 w3-center w3-quarter w3-white w3-padding-16 w3-border">
+    <img src="images/nft1.png" alt="Alps" style="width: auto; height: 300px;">
+    <div class="w3-container w3-center">
+      <p>NFT1</p>
+    </div>
+  </div>';
+  echo '<p class="w3-center w3-white w3-padding-small">'.$_SESSION['User_Name'].' balance:'.$_SESSION['Currency_Balance'].' coins, art pecies owned: '.$_SESSION['Owned_Art_Amount'].'</p>
+  </div>';
+    
+    
 }
 ?>
 <div class="w3-bar w3-border w3-light-grey">
-    <div class="w3-bar-item">NFTCAP</div>
+    <div class="w3-bar-item textbold">NFTCAP</div>
     <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?> method="post">
 <?php 
     // log in and register buttons get enabled
@@ -74,6 +97,69 @@ if(isset($_POST['MenuAction']))
 }
 
 ?>
+
+<!-- NFTs -->
+<div class="w3-row w3-padding w3-border w3-pale-blue w3-padding-16 w3-wide w3-large"  style="margin:auto;">
+
+<div class="w3-center textbold w3-padding-32">DIENOS PASIŪLYMAI</div>
+
+<div class="w3-card-3 w3-center w3-quarter w3-white w3-padding-16 w3-border">
+  <img src="images/nft1.png" alt="Alps" style="width: auto; height: 300px;">
+  <div class="w3-container w3-center">
+    <p>NFT1</p>
+  </div>
+</div>
+
+<div class="w3-card-3 w3-center w3-quarter w3-white w3-padding-16 w3-border">
+  <img src="images/nft2.png" alt="Alps" style="width: auto; height: 300px;">
+  <div class="w3-container w3-center">
+    <p>NFT2</p>
+  </div>
+</div>
+
+<div class="w3-card-3 w3-center w3-quarter w3-white w3-padding-16 w3-border">
+  <img src="images/nft3.png" alt="Alps" style="width: auto; height: 300px;">
+  <div class="w3-container w3-center">
+    <p>NFT3</p>
+  </div>
+</div>
+
+<div class="w3-card-3 w3-center w3-quarter w3-white w3-padding-16 w3-border">
+  <img src="images/nft4.png" alt="Alps" style="width: auto; height: 300px;">
+  <div class="w3-container w3-center">
+    <p>NFT4</p>
+  </div>
+</div>
+
+<div class="w3-card-3 w3-center w3-quarter w3-white w3-padding-16 w3-border">
+  <img src="images/nft5.png" alt="Alps" style="width: auto; height: 300px;">
+  <div class="w3-container w3-center">
+    <p>NFT5</p>
+  </div>
+</div>
+
+<div class="w3-card-3 w3-center w3-quarter w3-white w3-padding-16 w3-border">
+  <img src="images/nft6.png" alt="Alps" style="width: auto; height: 300px;">
+  <div class="w3-container w3-center">
+    <p>NFT6</p>
+  </div>
+</div>
+
+<div class="w3-card-3 w3-center w3-quarter w3-white w3-padding-16 w3-border">
+  <img src="images/nft7.png" alt="Alps" style="width: auto; height: 300px;">
+  <div class="w3-container w3-center">
+    <p>NFT7</p>
+  </div>
+</div>
+
+<div class="w3-card-3 w3-center w3-quarter w3-white w3-padding-16 w3-border">
+  <img src="images/nft8.png" alt="Alps" style="width: auto; height: 300px;">
+  <div class="w3-container w3-center">
+    <p>NFT8</p>
+  </div>
+</div>
+
+</div>
 <!-- Footer -->
 <footer class="w3-center w3-light-grey w3-topbar w3-padding-32">
   <p>NFTCAPⒸ2021. Visos teisės saugomos.</p>
