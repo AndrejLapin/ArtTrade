8e2a66b5a1dadde960a19f455390dbd39a8570bb
DESKTOP-LH76H01\Andrej <lapin.andrej1@gmail.com>
2021-12-14 23:53:40 +0200

    when importing db now creates itself

--- DB/arttradedb.sql
+++ DB/arttradedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2021 at 10:44 PM
+-- Generation Time: Dec 14, 2021 at 10:51 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `arttradedb`
 --
+CREATE DATABASE IF NOT EXISTS `arttradedb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `arttradedb`;
 
 -- --------------------------------------------------------
 
